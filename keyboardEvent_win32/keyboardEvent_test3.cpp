614edef1289c9163bc4e8f2b200cc603f65c3bca
chengkehan <chengkehan@gmail.com>
2012-06-08 00:06:37 +0800

    add GetAsyncKeyState test

---
+++ keyboardEvent_win32/keyboardEvent_test3.cpp
@@ -0,0 +1,96 @@
+#include <Windows.h>
+#include <WindowsX.h>
+
+#define KEY_DOWN(vkCode) ((GetAsyncKeyState(vkCode) & 0x8000) ? TRUE : FALSE)
+#define KEY_UP(vkCode) ((GetAsyncKeyState(vkCode) & 0x8000) ? FALSE : TRUE)
+
+#define WND_CLASS_NAME L"myWndClassName"
+#define WND_TITLE L"My Window Title"
+#define WND_X 0
+#define WND_Y 0
+#define WND_WIDTH 800
+#define WND_HEIGHT 600
+
+HWND hwnd;
+
+VOID MainLoop()
+{
+	if(KEY_DOWN(VK_LEFT))
+	{
+		OutputDebugString(L"VK_LEFT down\n");
+	}
+	/*if(KEY_UP(VK_LEFT))
+	{
+	OutputDebugString(L"VK_LEFT up\n");
+	}*/
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch(msg)
+	{
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			return 0;
+			break;
+		}
+
+		default:
+		{
+			break;
+		}
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+INT WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in LPSTR lpCmdLine, __in int nShowCmd )
+{
+	WNDCLASSEX wndClass;
+	memset(&wndClass, 0, sizeof(WNDCLASSEX));
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_DBLCLKS | CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = WindowProc;
+	wndClass.hInstance = hInstance;
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndClass.lpszClassName = WND_CLASS_NAME;
+
+	if(!RegisterClassEx(&wndClass))
+	{
+		return 0;
+	}
+
+	hwnd = CreateWindowEx(
+		NULL, 
+		WND_CLASS_NAME, WND_TITLE, 
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+		WND_X, WND_Y, WND_WIDTH, WND_HEIGHT, 
+		NULL, NULL, hInstance, NULL);
+	if(hwnd == NULL)
+	{
+		return 0;
+	}
+
+	ShowWindow(hwnd, nShowCmd);
+	UpdateWindow(hwnd);
+
+	MSG msg;
+	while(TRUE)
+	{
+		if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if(msg.message == WM_QUIT)
+			{
+				break;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		MainLoop();
+	}
+
+	return 0;
+}
