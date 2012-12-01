49e22c70f0bc21bc5d859e153577a6d8f252cc4d
chengkehan <chengkehan@gmail.com>
2012-12-01 22:11:13 +0800

    d3d JC2D

--- d3d/JC2D/JCRect.cpp
+++ d3d/JC2D/JCRect.cpp
@@ -22,3 +22,89 @@ JCRect::JCRect(CONST JCRect& rect)
 	width = rect.width;
 	height = rect.height;
 }
+
+BOOL JCRect::contains(FLOAT px, FLOAT py)
+{
+	return containsPoint(px, py);
+}
+
+BOOL JCRect::contains(JCRect* lpRect)
+{
+	if(lpRect == NULL)
+	{
+		return FALSE;
+	}
+	else
+	{
+		return containsPoint(lpRect->x, lpRect->y) && containsPoint(lpRect->x + lpRect->width, lpRect->y + lpRect->height);
+	}
+}
+
+BOOL JCRect::intersects(JCRect* lpRect)
+{
+	if(lpRect == NULL)
+	{
+		return FALSE;
+	}
+	else
+	{
+		FLOAT x1, x2, y1, y2;
+		return intersectoinInternal(lpRect, &x1, &x2, &y1, &y2);
+	}
+}
+
+JCRect* JCRect::intersection(JCRect* lpDestRect, JCRect* lpIntersectionResult)
+{
+	if(lpIntersectionResult == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		FLOAT x1, x2, y1, y2;
+		BOOL r = intersectoinInternal(lpDestRect, &x1, &x2, &y1, &y2);
+		if(!r)
+		{
+			return NULL;
+		}
+		else
+		{
+			lpIntersectionResult->x = x1;
+			lpIntersectionResult->width = x2 - x1;
+			lpIntersectionResult->y = y1;
+			lpIntersectionResult->height = y2 - y1;
+			return lpIntersectionResult;
+		}
+	}
+}
+
+JCRect* JCRect::combine(JCRect* lpDestRect, JCRect* lpCombineResult)
+{
+	if(lpDestRect == NULL || lpCombineResult == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		lpCombineResult->x = min(min(x, x + width), min(lpDestRect->x, lpDestRect->x + lpDestRect->width));
+		lpCombineResult->width = max(max(x, x + width), max(lpDestRect->x, lpDestRect->x + lpDestRect->width)) - lpCombineResult->x;
+		lpCombineResult->y = min(min(y, y + height), min(lpDestRect->y, lpDestRect->y + lpDestRect->height));
+		lpCombineResult->height = max(max(y, y + height), max(lpDestRect->y, lpDestRect->y + lpDestRect->height)) - lpCombineResult->y;
+		return lpCombineResult;
+	}
+}
+
+inline BOOL JCRect::containsPoint(FLOAT px, FLOAT py)
+{
+	return px >= x && px <= x + width && py >= y && py <= y + height;
+}
+
+inline BOOL JCRect::intersectoinInternal(JCRect* lpRect, FLOAT* x1, FLOAT* x2, FLOAT* y1 ,FLOAT* y2)
+{
+	*x1 = max(min(x, x + width), min(lpRect->x, lpRect->x + lpRect->width));
+	*x2 = min(min(x, x + width), min(lpRect->x, lpRect->x + lpRect->width));
+	*y1 = max(min(y, y + height), min(lpRect->y, lpRect->y + lpRect->height));
+	*y2 = min(min(y, y + height), min(lpRect->y, lpRect->y + lpRect->height));
+
+	return *x1 <= *x2 && *y1 <= *y2;
+}
--- d3d/JC2D/JCRect.h
+++ d3d/JC2D/JCRect.h
@@ -12,6 +12,16 @@ public:
 	JCRect(CONST JCRect& rect);
 
 	FLOAT x, y, width, height;
+
+	BOOL contains(FLOAT px, FLOAT py);
+	BOOL contains(JCRect* lpRect);
+	BOOL intersects(JCRect* lpRect);
+	JCRect* intersection(JCRect* lpDestRect, JCRect* lpIntersectionResult);
+	JCRect* combine(JCRect* lpDestRect, JCRect* lpCombineResult);
+
+private:
+	inline BOOL containsPoint(FLOAT px, FLOAT py);
+	inline BOOL intersectoinInternal(JCRect* lpDestRect, FLOAT* x1, FLOAT* x2, FLOAT* y1 ,FLOAT* y2);
 };
 
 #endif
